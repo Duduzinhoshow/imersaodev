80e928bdb14e4398264e48c85b38ababc2522954
Duduzinhoshow <eduardohrcoelhomkt@gmail.com>
2024-09-09 14:19:03 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Cruzeiro Esporte Clube",
+        descricao : "O Cruzeiro Esporte Clube é um dos maiores e mais tradicionais clubes de futebol do Brasil, com uma legião de apaixonados torcedores em Minas Gerais e em todo o país.",
+        link : "https://pt.wikipedia.org/wiki/Cruzeiro_Esporte_Clube",
+        tags: "minas cruzeiro cruzeirão cabuloso maior de minas"
+    },
+    {
+        titulo : "Flamengo",
+        descricao : "O Flamengo é um dos maiores clubes de futebol do mundo, conhecido por sua paixão e por sua torcida, a maior do Brasil. Aaaaaa",
+        link : "https://pt.wikipedia.org/wiki/Clube_de_Regatas_do_Flamengo",
+        tags: "mengão rio de janeiro malvadao"
+    },
+    {
+        titulo : "Corinthians",
+        descricao : "O Corinthians é um dos clubes de futebol mais populares do Brasil, com uma torcida apaixonada e uma história rica em títulos.",
+        link : "https://pt.wikipedia.org/wiki/Corinthians",
+        tags: "coringa fiel são paulo"
+    }
+];
+
